3d38f0bdf25d6e22afb5493116b92a684a1bbd0f
Alex Langevin <alexlangev@gmail.com>
2025-08-09 10:46:32 -0400

    loading spinner kinda worksgit add .!

--- internal/tui/connecting/connectingView.go
+++ internal/tui/connecting/connectingView.go
@@ -1,21 +1,65 @@
 package connecting
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"fmt"
+	"time"
 
-type Model struct{}
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Model struct {
+	isConnecting bool
+	spinner      spinner.Model
+}
+
+type connectedMsg struct{}
 
 func (m Model) Init() tea.Cmd {
-	return nil
+	// return m.spinner.Tick
+	return tea.Batch(
+		m.spinner.Tick,
+		mockConnection(),
+	)
 }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
-	return m, nil
+	switch msg := msg.(type) {
+	case connectedMsg:
+		m.isConnecting = false
+		return m, nil
+	default:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
 }
 
 func (m Model) View() string {
-	return "Connecting view\n"
+	s := ""
+
+	if m.isConnecting {
+		s = fmt.Sprintf("Trying to connect%s", m.spinner.View())
+	} else {
+		s = "Connection established!"
+	}
+
+	return s
 }
 
 func NewConnectingView() Model {
-	return Model{}
+	sp := spinner.New()
+	sp.Spinner = spinner.Ellipsis
+
+	return Model{
+		isConnecting: true,
+		spinner:      sp,
+	}
+}
+
+func mockConnection() tea.Cmd {
+	return func() tea.Msg {
+		time.Sleep(15 * time.Second)
+		return connectedMsg{}
+	}
 }
